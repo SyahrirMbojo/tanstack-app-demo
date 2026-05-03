fa6e6b50269432119a652d8401e6055694e2ea90
muh syahrir <muhsyahrir09@gmail.com>
2026-05-03 09:59:12 +0700

    update fixing vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,28 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
+import { defineConfig } from "vite";
+// import { devtools } from '@tanstack/devtools-vite'
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+let devtools: any = null;
+if (process.env.NODE_ENV !== "production") {
+  try {
+    const mod = require("@tanstack/devtools-vite");
+    devtools = mod.devtools;
+  } catch {}
+}
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
-})
+  // plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [
+    ...(devtools ? [devtools()] : []),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
+});
 
-export default config
+export default config;
