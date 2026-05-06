0a677a3fdda9a40e59932f9edce626378e6a3bfa
muh syahrir <muhsyahrir09@gmail.com>
2026-05-06 11:27:20 +0700

    Update fixing

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-// import { devtools } from "@tanstack/devtools-vite";
+import { devtools } from "@tanstack/devtools-vite";
 
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
@@ -7,28 +7,29 @@ import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import netlify from "@netlify/vite-plugin-tanstack-start";
 
-let devtools: any = null;
-if (process.env.NODE_ENV !== "production") {
-  try {
-    const mod = require("@tanstack/devtools-vite");
-    devtools = mod.devtools;
-  } catch {}
-}
-
-const config = defineConfig({
-  resolve: { tsconfigPaths: true },
-  plugins: [
-    ...(devtools ? [devtools()] : []),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-    netlify(),
-  ],
-});
+// let devtools: any = null;
+// if (process.env.NODE_ENV !== "production") {
+//   try {
+//     const mod = require("@tanstack/devtools-vite");
+//     devtools = mod.devtools;
+//   } catch {}
+// }
 
 // const config = defineConfig({
 //   resolve: { tsconfigPaths: true },
-//   plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact(), netlify()],
+//   // plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+//   plugins: [
+//     ...(devtools ? [devtools()] : []),
+//     tailwindcss(),
+//     tanstackStart(),
+//     viteReact(),
+//     netlify(),
+//   ],
 // });
+
+const config = defineConfig({
+  resolve: { tsconfigPaths: true },
+  plugins: [devtools(), netlify(), tailwindcss(), tanstackStart(), viteReact()],
+});
 
 export default config;
